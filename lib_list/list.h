ef6f11c20f398056a31dcbb876205cef31b2b42e
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2025-10-26 21:06:50 +0300

    added insert methods and tests

--- lib_list/list.h
+++ lib_list/list.h
@@ -26,13 +26,13 @@ public:
 
 	bool is_empty();
 
-	Node<T>* head();
-	Node<T>* tail();
+	Node<T>* head() const;
+	Node<T>* tail() const;
 
 	void push_front(const T&) noexcept;
 	void push_back(const T&) noexcept;
-	void insert(size_t, const T&) noexcept;
-	void insert(Node<T>*, const T&) noexcept;
+	void insert(size_t, const T&);
+	void insert(Node<T>*, const T&);
 
 	void pop_front();
 	void pop_back();
@@ -72,6 +72,60 @@ List<T>::~List() {
 		_head = _head->next;
 		delete node;
 	}
+}
+
+template <class T>
+bool List<T>::is_empty() { return _head == nullptr; }
+
+template <class T>
+Node<T>* List<T>::head() const { return _head; }
+
+template <class T>
+Node<T>* List<T>::tail() const { return _tail; }
+
+template <class T>
+void List<T>::push_front(const T& value) noexcept {
+	Node<T>* node = new Node<T>(value, _head);
+	if (is_empty()) { _tail = node; }
+	_head = node;
+	_count++;
+}
+
+template <class T>
+void List<T>::push_back(const T& value) noexcept {
+	Node<T>* node = new Node<T>(value);
+	if (is_empty()) { _tail = node; _head = node; _count++; return; }
+	_tail->next = node;
+	_tail = node;
+	_count++;
+}
+
+template <class T>
+void List<T>::insert(size_t pos, const T& value) {
+	if (pos > _count) throw std::logic_error("Position out of range");
+	if (pos == 0) { push_front(value); }
+	else if (pos == _count) { push_back(value); }
+	else {
+		Node<T>* curr = _head;
+		size_t curr_pos = 0;
+		while (curr != nullptr) {
+			if (curr_pos == pos - 1) break;
+			curr_pos++;
+			curr = curr->next;
+		}
+		insert(curr, value);
+		_count++;
+	}
+}
+
+template <class T>
+void List<T>::insert(Node<T>* node, const T& value) {
+	if (node == nullptr || is_empty()) throw std::logic_error("Incorrect input");
+	Node<T>* new_node = new Node<T>(value);
+	new_node->next = node->next;
+	node->next = new_node;
+	if (node == _tail) { _tail = new_node; }
+	_count++;
 }
 
 #endif  // LIB_LIST_LIST_H_
--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -11,10 +11,95 @@ TEST(TestListLib, can_create) {
 
 TEST(TestListLib, can_create_copy) {
 	List<int> list_1;
+
 	ASSERT_NO_THROW(List<int> list_2(list_1));
 }
 
 TEST(TestListLib, throw_when_try_create_copy) {
 	List<int>* list_1 = NULL;
+
 	ASSERT_ANY_THROW(List<int> list_2(*list_1));
+}
+
+TEST(TestListLib, can_check_empty_correctly) {
+	List<int> list;
+
+	EXPECT_EQ(true, list.is_empty());
+}
+
+TEST(TestListLib, can_push_front_correctly) {
+	List<int> list;
+
+	list.push_front(1);
+	list.push_front(2);
+	list.push_front(3);
+
+	EXPECT_EQ(1, list.tail()->value);
+	EXPECT_EQ(3, list.head()->value);
+}
+
+TEST(TestListLib, can_push_back_correctly) {
+	List<int> list;
+
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	EXPECT_EQ(3, list.tail()->value);
+	EXPECT_EQ(1, list.head()->value);
+}
+
+TEST(TestListLib, can_insert_by_pointer_correctly) {
+	List<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	list.insert(list.head()->next, 444);
+	list.insert(list.head(), 555);
+	list.insert(list.tail(), 666);
+
+	EXPECT_EQ(444, list.head()->next->next->next->value);
+	EXPECT_EQ(555, list.head()->next->value);
+	EXPECT_EQ(666, list.tail()->value);
+}
+
+TEST(TestListLib, throw_when_try_insert_by_pointer_empty) {
+	List<int> list;
+
+	ASSERT_ANY_THROW(list.insert(list.tail(), 555));
+}
+
+TEST(TestListLib, throw_when_try_insert_by_pointer_nullptr) {
+	List<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	Node<int>* ptr = nullptr;
+
+	ASSERT_ANY_THROW(list.insert(ptr, 555));
+}
+
+TEST(TestListLib, can_insert_by_pos_correctly) {
+	List<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	list.insert(1, 444);
+	list.insert((size_t)0, 555);
+	list.insert(5, 666);
+
+	EXPECT_EQ(444, list.head()->next->next->value);
+	EXPECT_EQ(555, list.head()->value);
+	EXPECT_EQ(666, list.tail()->value);
+}
+
+TEST(TestListLib, throw_when_try_insert_by_pos_out_of_range) {
+	List<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	ASSERT_ANY_THROW(list.insert(4, 555));
 }
