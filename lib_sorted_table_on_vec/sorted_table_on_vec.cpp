3de0c42df5f1207ade5058ccc170c187077b0a94
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2026-02-24 21:24:22 +0300

    added sorted table on vec and fixed code

---
+++ lib_sorted_table_on_vec/sorted_table_on_vec.cpp
@@ -0,0 +1,3 @@
+// Copyright 2026 Viktoria Buzlaeva
+
+#include "../lib_sorted_table_on_vec/sorted_table_on_vec.h"
--- lib_sorted_table_on_vec/sorted_table_on_vec.h
+++ lib_sorted_table_on_vec/sorted_table_on_vec.h
@@ -1,64 +1,66 @@
 // Copyright 2026 Viktoria Buzlaeva
 
-#ifndef LIB_UNSORTED_TABLE_ON_VEC_UNSORTED_TABLE_ON_VEC_H_
-#define LIB_UNSORTED_TABLE_ON_VEC_UNSORTED_TABLE_ON_VEC_H_
+#ifndef LIB_SORTED_TABLE_ON_VEC_SORTED_TABLE_ON_VEC_H_
+#define LIB_SORTED_TABLE_ON_VEC_SORTED_TABLE_ON_VEC_H_
 
 #include <iomanip>
 #include "../lib_table/table.h"
 #include "../lib_tvector/tvector.h"
 
 template <class TKey, class TValue>
-class UnsortedTableOnVec : public Table<TKey, TValue> {
+class SortedTableOnVec : public Table<TKey, TValue> {
     TVector<TPair<TKey, TValue>> _rows;
 
 public:
-    UnsortedTableOnVec() = default;
+    SortedTableOnVec() = default;
 
     void insert(const TKey&, const TValue&) override;
     void erase(const TKey&) override;
     TValue& found(const TKey&) override;
     bool is_empty() const noexcept override;
     void print() const noexcept override;
+
+private:
+    size_t find_index(const TKey&) const;
 };
 
 template <class TKey, class TValue>
-void UnsortedTableOnVec<TKey, TValue>::insert(const TKey& key, const TValue& value) {
-    TPair<TKey, TValue> new_pair(key, value);
-    if (find_first(_rows, new_pair) != -1) {
+void SortedTableOnVec<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+    size_t pos = find_index(key);
+    if (_rows[pos].key == key) {
         throw std::logic_error("Key already exists");
     }
-    _rows.push_back(new_pair);
+    TPair<TKey, TValue> new_pair(key, value);
+    _rows.insert(pos, new_pair);
 }
 
 template <class TKey, class TValue>
-void UnsortedTableOnVec<TKey, TValue>::erase(const TKey& key) {
+void SortedTableOnVec<TKey, TValue>::erase(const TKey& key) {
     if (is_empty()) throw std::logic_error("Table is empty");
-    TPair<TKey, TValue> new_pair(key, TValue());
-    int found = find_first(_rows, new_pair);
-    if (found == -1) {
-        throw std::logic_error("Key doesn't exist");
+    size_t pos = find_index(key);
+    if (_rows[pos].key != key) {
+        throw std::out_of_range("Key doesn't exist");
     }
-    _rows.erase(found);
+    _rows.erase(pos);
 }
 
 template <class TKey, class TValue>
-TValue& UnsortedTableOnVec<TKey, TValue>::found(const TKey& key) {
+TValue& SortedTableOnVec<TKey, TValue>::found(const TKey& key) {
     if (is_empty()) throw std::logic_error("Table is empty");
-    TPair<TKey, TValue> new_pair(key, TValue());
-    int found = find_first(_rows, new_pair);
-    if (found == -1) {
-        throw std::logic_error("Key doesn't exist");
+    size_t pos = find_index(key);
+    if (_rows[pos].key != key) {
+        throw std::out_of_range("Key doesn't exist");
     }
-    return _rows.at(found).value;
+    return _rows[pos].value;
 }
 
 template <class TKey, class TValue>
-bool UnsortedTableOnVec<TKey, TValue>::is_empty() const noexcept {
+bool SortedTableOnVec<TKey, TValue>::is_empty() const noexcept {
     return _rows.is_empty();
 }
 
 template <class TKey, class TValue>
-void UnsortedTableOnVec<TKey, TValue>::print() const noexcept {
+void SortedTableOnVec<TKey, TValue>::print() const noexcept {
     if (_rows.is_empty()) {
         std::cout << "Table is empty" << std::endl;
         return;
@@ -67,13 +69,13 @@ void UnsortedTableOnVec<TKey, TValue>::print() const noexcept {
     int key_length = 15;
     int value_length = 40;
 
-    std::cout << "+-" << std::string(key_length, '-') << "+-" 
+    std::cout << "+-" << std::string(key_length, '-') << "+-"
         << std::string(value_length, '-') << "+" << std::endl;
 
     std::cout << "| " << std::left << std::setw(key_length) << "Key" << "| "
         << std::setw(value_length) << "Value" << "|" << std::endl;
 
-    std::cout << "+-" << std::string(key_length, '-') << "+-" 
+    std::cout << "+-" << std::string(key_length, '-') << "+-"
         << std::string(value_length, '-') << "+" << std::endl;
 
     for (size_t i = 0; i < _rows.size(); i++) {
@@ -85,4 +87,19 @@ void UnsortedTableOnVec<TKey, TValue>::print() const noexcept {
         << std::string(value_length, '-') << "+" << std::endl;
 }
 
-#endif  // LIB_UNSORTED_TABLE_ON_VEC_UNSORTED_TABLE_ON_VEC_H_
+template <class TKey, class TValue>
+size_t SortedTableOnVec<TKey, TValue>::find_index(const TKey& key) const {
+    size_t left = 0, right = _rows.size();
+    while (left < right) {
+        size_t mid = left + (right - left) / 2;
+        if (_rows[mid].key < key)
+            left = mid + 1;
+        else if (key < _rows[mid].key)
+            right = mid;
+        else
+            return mid;
+    }
+    return left;
+}
+
+#endif  // LIB_SORTED_TABLE_ON_VEC_SORTED_TABLE_ON_VEC_H_
