b5af3216ac5b17121da55f0d1a3a3204ddb53128
ViktoriaBuzlaeva <vicktoria.buzlaeva@yandex.ru>
2026-03-18 17:56:40 +0300

    added bstree find and erase methods

--- lib_bstree/bstree.h
+++ lib_bstree/bstree.h
@@ -37,6 +37,8 @@ public:
 
 private:
     BSTNode<TKey, TValue>* find_parent(const TKey&) const noexcept;
+    void erase_node(BSTNode<TKey, TValue>*, BSTNode<TKey, TValue>*) noexcept;
+    BSTNode<TKey, TValue>* find_max_left(BSTNode<TKey, TValue>*) const noexcept;
     void clear_rec(BSTNode<TKey, TValue>*) noexcept;
 
     void print_DLCR_rec(BSTNode<TKey, TValue>*) const noexcept;
@@ -68,6 +70,40 @@ void BSTree<TKey, TValue>::insert(const TKey& key, const TValue& value) {
         return;
     }
     throw std::logic_error("Key already exists");
+}
+
+template <class TKey, class TValue>
+TValue* BSTree<TKey, TValue>::find(const TKey& key) const noexcept {
+    BSTNode<TKey, TValue>* parent = find_parent(key);
+    if (!parent) return nullptr;
+    if (parent->left && parent->left->data.key == key)
+        return &parent->left->data.value;
+    else if (parent->right && parent->right->data.key == key)
+        return &parent->right->data.value;
+    else if (parent == _root && _root->data.key == key)
+        return &_root->data.value;
+    return nullptr;
+}
+
+template <class TKey, class TValue>
+void BSTree<TKey, TValue>::erase(const TKey& key) {
+    BSTNode<TKey, TValue>* parent = find_parent(key);
+    BSTNode<TKey, TValue>* deleted_node = nullptr;
+
+    if (parent->data.key < key && parent->right) {
+        deleted_node = parent->right;
+    }
+    else if (parent->data.key > key && parent->left) {
+        deleted_node = parent->left;
+    }
+    else if (parent->data.key == key) {
+        deleted_node = parent;
+    }
+    else {
+        throw std::logic_error("Key doesn't exist");
+    }
+
+    erase_node(deleted_node, parent);
 }
 
 template <class TKey, class TValue>
@@ -103,6 +139,70 @@ BSTNode<TKey, TValue>* BSTree<TKey, TValue>::find_parent(const TKey& key) const 
             else curr = curr->right;
         }
     }
+}
+
+template <class TKey, class TValue>
+void BSTree<TKey, TValue>::erase_node(BSTNode<TKey, TValue>* deleted, BSTNode<TKey, TValue>* parent) noexcept {
+    if (!deleted->left && !deleted->right) {
+        if (deleted == _root) _root = nullptr;
+        else {
+            if (parent->data.key < deleted->data.key) {
+                parent->right = nullptr;
+            }
+            else {
+                parent->left = nullptr;
+            }
+        }
+        delete deleted;
+    }
+    else if (!deleted->left) {
+        if (deleted == _root) _root = deleted->right;
+        else {
+            if (parent->data.key < deleted->data.key) {
+                parent->right = deleted->right;
+            }
+            else {
+                parent->left = deleted->right;
+            }
+        }
+        delete deleted;
+    }
+    else if (!deleted->right) {
+        if (deleted == _root) _root = deleted->left;
+        else {
+            if (parent->data.key < deleted->data.key) {
+                parent->right = deleted->left;
+            }
+            else {
+                parent->left = deleted->left;
+            }
+        }
+        delete deleted;
+    }
+    else {
+        BSTNode<TKey, TValue>* replacer = find_max_left(deleted);
+        BSTNode<TKey, TValue>* replacer_parent = find_parent(replacer->data.key);
+        if (deleted == _root) _root->data = replacer->data;
+        else {
+            deleted->data = replacer->data;
+            if (parent->data.key < deleted->data.key) {
+                parent->right = deleted;
+            }
+            else {
+                parent->left = deleted;
+            }
+        }
+        erase_node(replacer, replacer_parent);
+    }
+}
+
+template <class TKey, class TValue>
+BSTNode<TKey, TValue>* BSTree<TKey, TValue>::find_max_left(BSTNode<TKey, TValue>* curr) const noexcept {
+    curr = curr->left;
+    while (curr->right) {
+        curr = curr->right;
+    }
+    return curr;
 }
 
 template <class TKey, class TValue>
--- tests/test_bstree.cpp
+++ tests/test_bstree.cpp
@@ -48,3 +48,73 @@ TEST(TestBSTreeLib, can_clear) {
     t.clear();
     EXPECT_TRUE(t.is_empty());
 }
+
+TEST(TestBSTreeLib, can_find) {
+    BSTree<int, std::string> t;
+    t.insert(1, "one");
+    EXPECT_EQ("one", *(t.find(1)));
+    EXPECT_EQ(nullptr, t.find(6));
+
+    t.insert(2, "two");
+    t.insert(5, "five");
+    t.insert(3, "three");
+    t.insert(4, "four");
+    EXPECT_EQ("one", *(t.find(1)));
+    EXPECT_EQ("five", *(t.find(5)));
+    EXPECT_EQ(nullptr, t.find(6));
+}
+
+TEST(TestBSTreeLib, can_erase) {
+    BSTree<int, std::string> t;
+    t.insert(1, "one");
+    t.insert(2, "two");
+    t.insert(5, "five");
+    t.insert(3, "three");
+    t.insert(4, "four");
+
+    t.erase(4); // нет предков
+    EXPECT_EQ(nullptr, t.find(4));
+
+    t.erase(5); // есть левый предок
+    EXPECT_EQ(nullptr, t.find(5));
+    EXPECT_EQ("three", *(t.find(3)));
+
+    t.erase(2); // есть правый предок
+    EXPECT_EQ(nullptr, t.find(2));
+    EXPECT_EQ("three", *(t.find(3)));
+
+    t.insert(8, "eight");
+    t.insert(9, "nine");
+    t.insert(4, "four");
+    t.insert(6, "six");
+    t.insert(5, "five");
+    t.erase(8); // есть оба предка
+    EXPECT_EQ(nullptr, t.find(8));
+    EXPECT_EQ("five", *(t.find(5)));
+    EXPECT_EQ("nine", *(t.find(9)));
+}
+
+TEST(TestBSTreeLib, can_erase_root) {
+    BSTree<int, std::string> t;
+    t.insert(1, "one");
+    t.erase(1); // нет предков
+    EXPECT_TRUE(t.is_empty());
+
+    t.insert(1, "one");
+    t.insert(2, "two");
+    t.erase(1); // есть правый предок
+    EXPECT_EQ(nullptr, t.find(1));
+    EXPECT_EQ("two", *(t.find(2)));
+
+    t.insert(1, "one");
+    t.erase(2); // есть левый предок
+    EXPECT_EQ(nullptr, t.find(2));
+    EXPECT_EQ("one", *(t.find(1)));
+
+    t.insert(2, "two");
+    t.insert(3, "three");
+    t.erase(1); // есть оба предка
+    EXPECT_EQ(nullptr, t.find(1));
+    EXPECT_EQ("two", *(t.find(2)));
+    EXPECT_EQ("three", *(t.find(3)));
+}
